de5018132a50cf7afd12a2d919fc5e75c38d210e
DSUN <dsun@hanmail.net>
2023-09-20 13:02:47 +0900

    AesEncrypt, AesDecrypt

--- src/A005.razor.cs
+++ src/A005.razor.cs
@@ -12,26 +12,123 @@ namespace MetaFrm.Management.Razor
         #endregion
 
 
-        internal Microsoft.AspNetCore.Components.Forms.IBrowserFile? DllFile { get; set; }
-        internal long ImageBytes;
-        internal string? ImageBase64Data;
+        private async Task JsonInputFileChangeEventArgs(Microsoft.AspNetCore.Components.Forms.InputFileChangeEventArgs e)
+        {
+            if (e.FileCount != 1)
+                return;
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptKey.Length < 5)
+            {
+                this.ToastShow("Key를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptIV.Length < 5)
+            {
+                this.ToastShow("IV를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            try
+            {
+                using MemoryStream memoryStream = new();
+                await e.File.OpenReadStream(10000 * 1024).CopyToAsync(memoryStream);
+                memoryStream.Position = 0;
+
+                using TextReader textReader = new StreamReader(memoryStream);
+
+                this.A005ViewModel.FirebaseAdminServiceAesEncryptString = textReader.ReadToEnd().AesEncryptToBase64String(this.A005ViewModel.FirebaseAdminServiceAesEncryptKey, this.A005ViewModel.FirebaseAdminServiceAesEncryptIV);
+                this.A005ViewModel.FirebaseAdminServiceAesDecryptString = this.A005ViewModel.FirebaseAdminServiceAesEncryptString.AesDecryptorToBase64String(this.A005ViewModel.FirebaseAdminServiceAesEncryptKey, this.A005ViewModel.FirebaseAdminServiceAesEncryptIV);
+            }
+            catch (Exception ex)
+            {
+                this.ToastShow(ex.Message);
+            }
+        }
+
+        private void OnAesDecryptClick()
+        {
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptKey.Length < 5)
+            {
+                this.ToastShow("Key를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptIV.Length < 5)
+            {
+                this.ToastShow("IV를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptString.Length < 5)
+            {
+                this.ToastShow("AesEncrypt String을 5자리 이상 입력하세요.");
+                return;
+            }
+
+            try
+            {
+                this.A005ViewModel.FirebaseAdminServiceAesDecryptString = this.A005ViewModel.FirebaseAdminServiceAesEncryptString.AesDecryptorToBase64String(this.A005ViewModel.FirebaseAdminServiceAesEncryptKey, this.A005ViewModel.FirebaseAdminServiceAesEncryptIV);
+            }
+            catch (Exception ex)
+            {
+                this.ToastShow(ex.Message);
+            }
+        }
+        private void OnAesEncryptClick()
+        {
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptKey.Length < 5)
+            {
+                this.ToastShow("Key를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesEncryptIV.Length < 5)
+            {
+                this.ToastShow("IV를 5자리 이상 입력하세요.");
+                return;
+            }
+
+            if (this.A005ViewModel.FirebaseAdminServiceAesDecryptString.Length < 5)
+            {
+                this.ToastShow("AesDecrypt String을 5자리 이상 입력하세요.");
+                return;
+            }
+
+            try
+            {
+                this.A005ViewModel.FirebaseAdminServiceAesEncryptString = this.A005ViewModel.FirebaseAdminServiceAesDecryptString.AesEncryptToBase64String(this.A005ViewModel.FirebaseAdminServiceAesEncryptKey, this.A005ViewModel.FirebaseAdminServiceAesEncryptIV);
+            }
+            catch (Exception ex)
+            {
+                this.ToastShow(ex.Message);
+            }
+        }
+
         private async Task InputFileChangeEventArgs(Microsoft.AspNetCore.Components.Forms.InputFileChangeEventArgs e)
         {
+            if (e.FileCount != 1)
+                return;
+
             byte[] bytes;
 
-
-            if (e.FileCount == 1)
+            try
             {
-                this.DllFile = e.File;
-                this.ImageBytes = this.DllFile.Size;
+                this.A005ViewModel.DllFile = e.File;
+                this.A005ViewModel.ImageBytes = this.A005ViewModel.DllFile.Size;
 
-                MemoryStream memoryStream = new();
-                await this.DllFile.OpenReadStream(10000 * 1024).CopyToAsync(memoryStream);
+                using MemoryStream memoryStream = new();
+                await this.A005ViewModel.DllFile.OpenReadStream(10000 * 1024).CopyToAsync(memoryStream);
                 memoryStream.Position = 0;
-                bytes = new byte[this.DllFile.Size];
-                int count = await memoryStream.ReadAsync(bytes.AsMemory(0, (int)this.DllFile.Size));
+                
+                bytes = new byte[this.A005ViewModel.DllFile.Size];
+                _ = await memoryStream.ReadAsync(bytes.AsMemory(0, (int)this.A005ViewModel.DllFile.Size));
 
-                this.ImageBase64Data = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(bytes));
+                this.A005ViewModel.ImageBase64Data = string.Format("data:image/png;base64,{0}", Convert.ToBase64String(bytes));
+            }
+            catch (Exception ex)
+            {
+                this.ToastShow(ex.Message);
             }
         }
     }
--- src/ViewModels/A005ViewModel.cs
+++ src/ViewModels/A005ViewModel.cs
@@ -8,6 +8,78 @@ namespace MetaFrm.Management.Razor.ViewModels
     public partial class A005ViewModel : BaseViewModel
     {
         /// <summary>
+        /// FirebaseAdminServiceAesEncryptKey
+        /// </summary>
+        public string FirebaseAdminServiceAesEncryptKey { get; set; } = string.Empty;
+
+        /// <summary>
+        /// FirebaseAdminServiceAesEncryptIV
+        /// </summary>
+        public string FirebaseAdminServiceAesEncryptIV { get; set; } = string.Empty;
+
+        /// <summary>
+        /// FirebaseAdminServiceAesEncryptString
+        /// </summary>
+        public string FirebaseAdminServiceAesEncryptString { get; set; } = string.Empty;
+
+        /// <summary>
+        /// FirebaseAdminServiceAesDecryptString
+        /// </summary>
+        public string FirebaseAdminServiceAesDecryptString { get; set; } = string.Empty;
+
+        /// <summary>
+        /// FirebaseAdminServiceAesEncryptString1
+        /// </summary>
+        public string FirebaseAdminServiceAesEncryptString1 
+        {
+            get
+            {
+                if (this.FirebaseAdminServiceAesEncryptString.Length > 4000)
+                    return this.FirebaseAdminServiceAesEncryptString[..4000];
+                else
+                    return this.FirebaseAdminServiceAesEncryptString;
+            }
+            set
+            {
+                this.FirebaseAdminServiceAesEncryptString = value;
+            }
+        }
+
+        /// <summary>
+        /// FirebaseAdminServiceAesEncryptString2
+        /// </summary>
+        public string FirebaseAdminServiceAesEncryptString2
+        {
+            get
+            {
+                if (this.FirebaseAdminServiceAesEncryptString.Length > 4000)
+                    return this.FirebaseAdminServiceAesEncryptString[4000..];
+                else
+                    return "";
+            }
+            set
+            {
+                this.FirebaseAdminServiceAesEncryptString += value;
+            }
+        }
+
+
+        /// <summary>
+        /// DllFile
+        /// </summary>
+        public Microsoft.AspNetCore.Components.Forms.IBrowserFile? DllFile { get; set; }
+
+        /// <summary>
+        /// ImageBytes
+        /// </summary>
+        public long ImageBytes { get; set; }
+
+        /// <summary>
+        /// ImageBase64Data
+        /// </summary>
+        public string? ImageBase64Data { get; set; }
+
+        /// <summary>
         /// A005ViewModel
         /// </summary>
         public A005ViewModel()
